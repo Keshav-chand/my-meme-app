3056f407f744625763a72acd6606c9aefcc8bd8d
Keshav chand <Keshavchand204@gmail.com>
2024-08-21 21:59:02 +0530

    first changes

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+      <footer>
+          <p>
+              Made by:
+
+              <a className="website-link" href="https://x.com/Keshav_codes_">
+                Keshav_codes_
+              </a>
+              
+          </p>
+      </footer>
+  );
+}
+
+export default Footer;
